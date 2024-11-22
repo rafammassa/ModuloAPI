a004f75287e06a4d451d5f3df59457a292e8e344
rafammassa <rafaella_massa@hotmail.com>
2024-11-22 18:41:55 -0300

    Criando o endpoint ObterPorNome

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -38,6 +38,13 @@ namespace ModuloAPI.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Contato contato)
         {
